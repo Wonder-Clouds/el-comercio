e0509e34c4e4f8522a561ba6b6429b01115f0493
Cristian Monzon <cristian-02-@live.com>
2025-01-24 00:00:39 -0500

    Terminando la configuracion para el backend

---
+++ backend/wait_for_db.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Espera a que la base de datos esté disponible
+until nc -z -v -w30 db 5432
+do
+  echo "Esperando a que la base de datos esté disponible..."
+  sleep 1
+done
+
+echo "Base de datos lista. Ejecutando migraciones..."
+
+# Ejecutar makemigrations solo para las aplicaciones específicas que se indican
+python manage.py makemigrations assignment detail_assignment devolution product product_price seller --noinput
+
+# Ejecutar migrate para aplicar las migraciones pendientes de las aplicaciones especificadas
+python manage.py migrate --noinput
