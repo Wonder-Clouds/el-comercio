2712b63547229d94476c7a2ad9885952b5cfab77
zsalazarq <salazarsergio1082@gmail.com>
2025-01-30 00:34:12 -0500

    [update] Fixed refresh token and swagger auth

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -1,5 +1,6 @@
 // src/utils/axios.ts
-import axios from "axios";
+import { RefreshToken } from "@/api/Token.api";
+import axios, { AxiosError } from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -17,6 +18,37 @@ api.interceptors.request.use(
     return config;
   },
   (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error: AxiosError) => {
+    const originalRequest = error.config;
+    
+    if (
+      error.response?.status === 401 &&
+      originalRequest?.url !== "/token/refresh/" &&
+      originalRequest
+    ) {
+      try {
+        const refreshToken = localStorage.getItem("refreshToken");
+        const response = await RefreshToken(refreshToken as string);
+
+        const { access } = response.data;
+        localStorage.setItem("accessToken", access);
+
+        originalRequest.headers.Authorization = `Bearer ${access}`;
+        return axios(originalRequest);
+      } catch (refreshError) {
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("refreshToken");
+        window.location.href = "/login";
+        return Promise.reject(refreshError);
+      }
+    }
+
     return Promise.reject(error);
   }
 );
