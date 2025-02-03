c86e1d7dedcef85e1416a71a16d3e006d0e77cc8
Cristian Monzon <cristian-02-@live.com>
2025-02-02 21:46:36 -0500

    Elimando product_price de las migraciones

--- backend/wait_for_db.sh
+++ backend/wait_for_db.sh
@@ -10,7 +10,7 @@ done
 echo "Base de datos lista. Ejecutando migraciones..."
 
 # Ejecutar makemigrations solo para las aplicaciones específicas que se indican
-python manage.py makemigrations assignment detail_assignment devolution product product_price seller --noinput
+python manage.py makemigrations assignment detail_assignment devolution product seller --noinput
 
 # Ejecutar migrate para aplicar las migraciones pendientes de las aplicaciones especificadas
 python manage.py migrate --noinput
