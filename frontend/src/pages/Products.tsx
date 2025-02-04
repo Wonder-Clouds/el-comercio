0d22d76a4c39be1b8a11759bda1d72e8824a116e
zsalazarq <salazarsergio1082@gmail.com>
2025-02-03 21:49:19 -0500

    Updated the product page

--- frontend/src/pages/Products.tsx
+++ frontend/src/pages/Products.tsx
@@ -1,18 +1,21 @@
 import { ProductTable } from "@/components/products/ProductTable";
 import { Button } from "@/components/ui/button";
-import { columns } from "../components/products/columns";
-import { Product } from "@/models/Product";
+import { Product, ProductType } from "@/models/Product";
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { FileDown, Printer, Search, UserPlus, X } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { getProducts } from "@/api/Product.api";
 import { debounce } from "lodash";
 import printElement from "@/utils/printElement";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const Products = () => {
   const tableRef = useRef<HTMLDivElement>(null);
-  
-  const [data, setData] = useState<Product[]>([]);
+
+  const [activeTab, setActiveTab] = useState("products");
+
+  const [products, setProducts] = useState<Product[]>([]);
+  const [newspapers, setNewspapers] = useState<Product[]>([]);
   const [page, setPage] = useState(1);
   const [pageSize] = useState(10);
   const [totalCount, setTotalCount] = useState(0);
@@ -20,11 +23,23 @@ const Products = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [isSearching, setIsSearching] = useState(false);
 
-  const fetchData = useCallback(async () => {
+  const fetchProduct = useCallback(async () => {
     try {
-      const products = await getProducts(page, pageSize);
-      setData(products.results);
+      const products = await getProducts(page, pageSize, ProductType.PRODUCT);
+      setProducts(products.results);
       setTotalCount(products.count);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
+  }, [page, pageSize]);
+
+  const fetchNewspapers = useCallback(async () => {
+    try {
+      const newspapers = await getProducts(page, pageSize, ProductType.NEWSPAPER);
+      setNewspapers(newspapers.results);
+      setTotalCount(newspapers.count);
     } catch (error) {
       if (error instanceof Error) {
         console.error(error.message);
@@ -35,36 +50,56 @@ const Products = () => {
   // Debounce search
   const debouncedSearch = useMemo(
     () => debounce(async (productName: string) => {
-      if (productName) {
-        setIsSearching(true);
-        try {
-          const products = await getProducts(1, pageSize, undefined, productName);
-          setData(products.results);
-          setTotalCount(products.count);
-          setPage(1);
-        } catch (error) {
-          if (error instanceof Error) {
-            console.error(error.message);
+      if (activeTab === "newspapers") {
+        if (productName) {
+          setIsSearching(true);
+          try {
+            const products = await getProducts(1, pageSize, ProductType.PRODUCT, productName);
+            setProducts(products.results);
+            setTotalCount(products.count);
+            setPage(1);
+          } catch (error) {
+            if (error instanceof Error) {
+              console.error(error.message);
+            }
           }
+          setIsSearching(false);
+        } else {
+          fetchProduct();
         }
-        setIsSearching(false);
       } else {
-        fetchData();
+        if (productName) {
+          setIsSearching(true);
+          try {
+            const newspapers = await getProducts(1, pageSize, ProductType.NEWSPAPER, productName);
+            setNewspapers(newspapers.results);
+            setTotalCount(newspapers.count);
+            setPage(1);
+          } catch (error) {
+            if (error instanceof Error) {
+              console.error(error.message);
+            }
+          }
+          setIsSearching(false);
+        } else {
+          fetchNewspapers();
+        }
       }
     }, 300),
-    [pageSize, fetchData]
+    [pageSize, activeTab, fetchProduct, fetchNewspapers]
   );
 
   useEffect(() => {
     if (!searchTerm) {
-      fetchData();
+      fetchProduct();
+      fetchNewspapers();
     } else {
       debouncedSearch(searchTerm);
     }
     return () => {
       debouncedSearch.cancel();
     };
-  }, [page, pageSize, searchTerm, debouncedSearch, fetchData]);
+  }, [page, pageSize, searchTerm, debouncedSearch, fetchProduct, fetchNewspapers]);
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
@@ -86,62 +121,125 @@ const Products = () => {
   };
 
   return (
-    <div className="container mx-auto mt-6 space-y-6">
-      <div className="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
-        <h1 className="text-4xl font-bold">Periodicos</h1>
-        {/* Export buttons */}
-        <div className="flex flex-wrap gap-3">
-          <Button variant="outline" className="flex items-center gap-2">
-            <FileDown className="w-4 h-4" />
-            Exportar
-          </Button>
-          <Button onClick={handlePrint} variant="outline" className="flex items-center gap-2">
-            <Printer className="w-4 h-4" />
-            Imprimir
-          </Button>
-          <Button className="flex items-center gap-2">
-            <UserPlus className="w-4 h-4" />
-            Nuevo producto
-          </Button>
+    <Tabs defaultValue="products" className="mx-auto space-y-6" onValueChange={(value) => setActiveTab(value)}>
+      <TabsList className="flex bg-gray-900 rounded-none py-7">
+        <TabsTrigger value="products" className="px-5 text-lg data-[state=active]:text-black text-white">Productos</TabsTrigger>
+        <TabsTrigger value="newspapers" className="px-5 text-lg data-[state=active]:text-black text-white">Periódicos</TabsTrigger>
+      </TabsList>
+
+      <TabsContent value="products" className="container space-y-5 mx-auto">
+        <div className="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
+          <h1 className="text-4xl font-bold">Productos</h1>
+          {/* Export buttons */}
+          <div className="flex flex-wrap gap-3">
+            <Button variant="outline" className="flex items-center gap-2">
+              <FileDown className="w-4 h-4" />
+              Exportar
+            </Button>
+            <Button onClick={handlePrint} variant="outline" className="flex items-center gap-2">
+              <Printer className="w-4 h-4" />
+              Imprimir
+            </Button>
+            <Button className="flex items-center gap-2">
+              <UserPlus className="w-4 h-4" />
+              Nuevo producto
+            </Button>
+          </div>
         </div>
-      </div>
-      {/* Search Bar */}
-      <div className="relative">
+        {/* Search Bar */}
         <div className="relative">
-          <Search className="absolute w-4 h-4 text-gray-500 -translate-y-1/2 left-3 top-1/2" />
-          <Input
-            type="text"
-            placeholder="Buscar producto por nombre"
-            value={searchTerm}
-            onChange={handleSearch}
-            className="pl-10 pr-10"
-          />
-          {searchTerm && (
-            <button
-              onClick={clearSearch}
-              className="absolute text-gray-500 -translate-y-1/2 right-3 top-1/2 hover:text-gray-700"
-            >
-              <X className="w-4 h-4" />
-            </button>
+          <div className="relative">
+            <Search className="absolute w-4 h-4 text-gray-500 -translate-y-1/2 left-3 top-1/2" />
+            <Input
+              type="text"
+              placeholder="Buscar producto por nombre"
+              value={searchTerm}
+              onChange={handleSearch}
+              className="pl-10 pr-10"
+            />
+            {searchTerm && (
+              <button
+                onClick={clearSearch}
+                className="absolute text-gray-500 -translate-y-1/2 right-3 top-1/2 hover:text-gray-700"
+              >
+                <X className="w-4 h-4" />
+              </button>
+            )}
+          </div>
+          {isSearching && (
+            <div className="absolute -translate-y-1/2 right-14 top-1/2">
+              <div className="w-4 h-4 border-2 border-gray-300 rounded-full animate-spin border-t-gray-600" />
+            </div>
           )}
         </div>
-        {isSearching && (
-          <div className="absolute -translate-y-1/2 right-14 top-1/2">
-            <div className="w-4 h-4 border-2 border-gray-300 rounded-full animate-spin border-t-gray-600" />
-          </div>
-        )}
-      </div>
 
-      <ProductTable
-        columns={columns}
-        data={data}
-        page={page}
-        pageSize={pageSize}
-        totalCount={totalCount}
-        onPageChange={handlePageChange}
-        tableRef={tableRef}
-      />
-    </div>
+        <ProductTable
+          data={products}
+          page={page}
+          pageSize={pageSize}
+          totalCount={totalCount}
+          onPageChange={handlePageChange}
+          tableRef={tableRef}
+        />
+      </TabsContent>
+
+      <TabsContent value="newspapers" className="container space-y-5 mx-auto">
+        <div className="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
+          <h1 className="text-4xl font-bold">Periodicos</h1>
+          {/* Export buttons */}
+          <div className="flex flex-wrap gap-3">
+            <Button variant="outline" className="flex items-center gap-2">
+              <FileDown className="w-4 h-4" />
+              Exportar
+            </Button>
+            <Button onClick={handlePrint} variant="outline" className="flex items-center gap-2">
+              <Printer className="w-4 h-4" />
+              Imprimir
+            </Button>
+            <Button className="flex items-center gap-2">
+              <UserPlus className="w-4 h-4" />
+              Nuevo periodico
+            </Button>
+          </div>
+        </div>
+        {/* Search Bar */}
+        <div className="relative">
+          <div className="relative">
+            <Search className="absolute w-4 h-4 text-gray-500 -translate-y-1/2 left-3 top-1/2" />
+            <Input
+              type="text"
+              placeholder="Buscar producto por nombre"
+              value={searchTerm}
+              onChange={handleSearch}
+              className="pl-10 pr-10"
+            />
+            {searchTerm && (
+              <button
+                onClick={clearSearch}
+                className="absolute text-gray-500 -translate-y-1/2 right-3 top-1/2 hover:text-gray-700"
+              >
+                <X className="w-4 h-4" />
+              </button>
+            )}
+          </div>
+          {isSearching && (
+            <div className="absolute -translate-y-1/2 right-14 top-1/2">
+              <div className="w-4 h-4 border-2 border-gray-300 rounded-full animate-spin border-t-gray-600" />
+            </div>
+          )}
+        </div>
+
+        <ProductTable
+          data={newspapers}
+          page={page}
+          pageSize={pageSize}
+          totalCount={totalCount}
+          onPageChange={handlePageChange}
+          tableRef={tableRef}
+        />
+      </TabsContent>
+
+    </Tabs>
   );
 }
 
