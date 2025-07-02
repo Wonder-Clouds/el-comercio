4459ed2a798081ffc441374faa636d26c301a30c
Cristian Monzon <cristian-02-@live.com>
2025-07-02 16:58:44 -0500

    Corrigiendo el swagger para produccion

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,8 @@
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+import os
+from dotenv import load_dotenv
 from django.urls import path
 
 # Create a schema view for the API documentation using drf_yasg
@@ -14,8 +16,13 @@ schema_view = get_schema_view(
     permission_classes=[permissions.AllowAny]
 )
 
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 # Define URL patterns for the API documentation
-urlpatterns = [
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+
+urlpatterns = []
+
+if DEBUG:
+    urlpatterns = [
+        path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    ]
